b4f6f8d76d04403744682bb34688c047d753dcee
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-12-28 21:17:51 -0800

    final push

--- script.js
+++ script.js
@@ -1,89 +1,31 @@
-// save reference to important DOM elements
+document.addEventListener("DOMContentLoaded", function(){
 var timeDisplayEl = $('#time-display');
-var projectDisplayEl = $('#project-display');
-var projectModalEl = $('#project-modal');
-var projectFormEl = $('#project-form');
-var projectNameInputEl = $('#project-name-input');
-var projectTypeInputEl = $('#project-type-input');
-var hourlyRateInputEl = $('#hourly-rate-input');
-var dueDateInputEl = $('#due-date-input');
 
-// handle displaying the time
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
 
-// handle printing project data to the page
-function printProjectData(name, type, hourlyRate, dueDate) {
-  var projectRowEl = $('<tr>');
+function hourCheck() {
+  let currentTime=moment().format('HH');
+  console.log(currentTime);
+  let hourArray=document.querySelectorAll('.hour');
+  console.log(hourArray);
+  for (let index = 0; index < hourArray.length; index++) {
+    console.log(hourArray[index].getAttribute('data-hour'));
+    if (hourArray[index].getAttribute('data-hour') < currentTime) {
+      hourArray[index].classList.add('past');
 
-  var projectNameTdEl = $('<td>').addClass('p-2').text(name);
+    }else if (hourArray[index].getAttribute('data-hour') == currentTime) {
+      hourArray[index].classList.add('present');
 
-  var projectTypeTdEl = $('<td>').addClass('p-2').text(type);
-
-  var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
-
-  var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
-
-  var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
-  var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
-
-  var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
-
-  // You can also chain methods onto new lines to keep code clean
-  var totalTdEl = $('<td>')
-    .addClass('p-2')
-    .text('$' + totalEarnings);
-
-  var deleteProjectBtn = $('<td>')
-    .addClass('p-2 delete-project-btn text-center')
-    .text('X');
-
-  // By listing each `<td>` variable as an argument, each one will be appended in that order
-  projectRowEl.append(
-    projectNameTdEl,
-    projectTypeTdEl,
-    rateTdEl,
-    dueDateTdEl,
-    daysLeftTdEl,
-    totalTdEl,
-    deleteProjectBtn
-  );
-
-  projectDisplayEl.append(projectRowEl);
-
-  projectModalEl.modal('hide');
+    }else if (hourArray[index].getAttribute('data-hour') > currentTime) {
+      hourArray[index].classList.add('future');
+      console.log(hourArray[index].getAttribute('data-hour'));  
+    }
+  }
 }
 
-function calculateTotalEarnings(rate, days) {
-  var dailyTotal = rate * 8;
-  var total = dailyTotal * days;
-  return total;
-}
-
-function handleDeleteProject(event) {
-  console.log(event.target);
-  var btnClicked = $(event.target);
-  btnClicked.parent('tr').remove();
-}
-
-// handle project form submission
-function handleProjectFormSubmit(event) {
-  event.preventDefault();
-
-  var projectName = projectNameInputEl.val().trim();
-  var projectType = projectTypeInputEl.val().trim();
-  var hourlyRate = hourlyRateInputEl.val().trim();
-  var dueDate = dueDateInputEl.val().trim();
-
-  printProjectData(projectName, projectType, hourlyRate, dueDate);
-
-  projectFormEl[0].reset();
-}
-
-projectFormEl.on('submit', handleProjectFormSubmit);
-projectDisplayEl.on('click', '.delete-project-btn', handleDeleteProject);
-dueDateInputEl.datepicker({ minDate: 1 });
-
+hourCheck();
 setInterval(displayTime, 1000);
+});
