200fcf1fa270ef7e3b291061b93dc01a7dba6540
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-12-29 21:15:48 -0800

    saveText function, save(event) function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var saveBtn = document.getElementsByTagName('button');
+// saveBtn.addEventListener("click", save());
+
+
 document.addEventListener("DOMContentLoaded", function(){
 var timeDisplayEl = $('#time-display');
 
@@ -7,12 +11,11 @@ function displayTime() {
 }
 
 function hourCheck() {
+
   let currentTime=moment().format('HH');
-  console.log(currentTime);
   let hourArray=document.querySelectorAll('.hour');
-  console.log(hourArray);
+
   for (let index = 0; index < hourArray.length; index++) {
-    console.log(hourArray[index].getAttribute('data-hour'));
     if (hourArray[index].getAttribute('data-hour') < currentTime) {
       hourArray[index].classList.add('past');
 
@@ -21,11 +24,43 @@ function hourCheck() {
 
     }else if (hourArray[index].getAttribute('data-hour') > currentTime) {
       hourArray[index].classList.add('future');
-      console.log(hourArray[index].getAttribute('data-hour'));  
+    }
+  }
+}
+function save(event) {
+  let scheduleValue=event.target.parentElement.previousElementSibling.children[0].value;
+  let scheduleKey=event.target.parentElement.previousElementSibling.children[0].getAttribute('data-description');
+  console.log(scheduleValue);
+  localStorage.setItem(scheduleKey, scheduleValue);
+}
+
+let btnArray = document.querySelectorAll('.saveBtn')
+
+for (let index = 0; index < btnArray.length; index++) {
+  btnArray[index].addEventListener('click', function(event) {
+   // console.log(event.target.parentElement.previousElementSibling.children[0].getAttribute('data-description'));
+    save(event)
+  });
+  
+}
+
+//make into savetext function
+function saveText() {
+  let textArray=document.querySelectorAll('.description')
+  //put all text areas in an array
+  for (let index = 0; index < textArray.length; index++) {
+    textArray[index];
+    //then if not, don't display anything
+    if (localStorage.getItem(textArray)===null) {
+      //then check if there's something for it in local storage, possibly give id value that matches key
+    } else {
+      //if there is, then grab the local storage value and set the text area's value = to that
+      localStorage.setItem(scheduleKey, scheduleValue)
     }
   }
 }
 
+saveText();
 hourCheck();
 setInterval(displayTime, 1000);
 });
